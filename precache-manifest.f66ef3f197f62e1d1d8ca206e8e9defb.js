18761461af7a39dcc512be450f1d0a5bfb8045d6
VincentChretien <chretien.vincent.01@gmail.com>
2023-08-31 21:07:58 -0400

    Updates

---
+++ precache-manifest.f66ef3f197f62e1d1d8ca206e8e9defb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5703bc44e3bed9e9c7c",
+    "url": "/static/css/main.3cb2ec29.chunk.css"
+  },
+  {
+    "revision": "f5703bc44e3bed9e9c7c",
+    "url": "/static/js/main.f5703bc4.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "cc238d45167a0b4d79d934fc5d4e6db5",
+    "url": "/index.html"
+  }
+];
